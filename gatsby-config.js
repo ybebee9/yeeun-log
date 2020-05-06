46b9f483b1b746d225b61311029f3c4e2103ecd9
김예은 <kimyeeun@Agatha-MacBookPro.local>
2020-05-06 23:35:33 +0900

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,71 @@
+let siteConfig;
+let ghostConfig;
+
+try {
+  siteConfig = require(`./siteConfig`);
+} catch (e) {
+  siteConfig = null;
+}
+
+try {
+  ghostConfig = require(`./.ghost`);
+} catch (e) {
+  ghostConfig = {
+    development: {
+      apiUrl: process.env.GHOST_API_URL,
+      contentApiKey: process.env.GHOST_CONTENT_API_KEY,
+      version: process.env.GHOST_VERSION
+    },
+    production: {
+      apiUrl: process.env.GHOST_API_URL,
+      contentApiKey: process.env.GHOST_CONTENT_API_KEY,
+      version: process.env.GHOST_VERSION
+    }
+  };
+} finally {
+  const { apiUrl, contentApiKey } =
+    process.env.NODE_ENV === `development`
+      ? ghostConfig.development
+      : ghostConfig.production;
+
+  if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
+    ghostConfig = null; //allow default config to take over
+  }
+}
+
+let gatsbyPlugins = [
+  {
+    resolve: `@draftbox-co/gatsby-theme-ghost-attila`,
+    options: {
+      ghostConfig: ghostConfig,
+      siteConfig: siteConfig
+    }
+  }
+]
+
+if(process.env.SEGMENT_KEY) {
+  gatsbyPlugins.push({
+    resolve: `gatsby-plugin-segment-js`,
+    options: {
+      prodKey: process.env.SEGMENT_KEY,
+      devKey: process.env.SEGMENT_KEY,
+      trackPage: true,
+      delayLoad: true,
+      delayLoadTime: 1000
+    }
+  });
+}
+
+if(process.env.GA) {
+  gatsbyPlugins.unshift({
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GA,
+      head: true
+    }
+  });
+}
+
+module.exports = {
+  plugins: gatsbyPlugins
+};
